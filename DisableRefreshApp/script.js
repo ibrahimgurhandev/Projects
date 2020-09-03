40977ab0d56986ef3812f95790e9e48d359cbb83
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-03 18:34:30 -0400

    update script

--- DisableRefreshApp/script.js
+++ DisableRefreshApp/script.js
@@ -5,7 +5,7 @@ function actions() {
   //can put alert here or leave empty for now
   //   alert("Leon this is not the website you wanted to refresh");
   // or
-  alert("This is not a website you should refresh");
+  // alert("This is not a website you should refresh");
 }
 
 // simultaneous pressing Ctrl + R
