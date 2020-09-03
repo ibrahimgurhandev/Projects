73687eb1596bb792ea5ea17d9db8aafae4237fac
ibrahimgurhandev <ibrahimgurhan.dev@gmail.com>
2020-09-03 10:37:29 -0400

    update chrome extension

--- DisableRefreshApp/script.js
+++ DisableRefreshApp/script.js
@@ -1,17 +1,17 @@
-document.addEventListener("keydown", keysDown(actions));
+window.addEventListener("keydown", keysDown(actions));
 
 function actions() {
   // action that will happen when keys pressed
   //can put alert here or leave empty for now
   //   alert("Leon this is not the website you wanted to refresh");
   // or
-  // alert("This is not a website you should refresh");
+  alert("This is not a website you should refresh");
 }
 
 // simultaneous pressing Ctrl + R
 function keysDown() {
   return function (e) {
-    if (e.ctrlKey && e.code === "KeyR") {
+    if ((e.ctrlKey && e.code === "KeyR") || (e.metaKey && e.code === "KeyR")) {
       e.preventDefault();
       console.log(e);
       console.log(" --- Refresh shortcut sucessfully blocked ---");
